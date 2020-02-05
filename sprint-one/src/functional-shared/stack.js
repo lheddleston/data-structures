19f731489a41c9a7cc7a5afdbf4e0255700db9a7
Laura Heddleston <laura.heddleston@gmail.com>
2020-02-05 12:14:05 -0800

    Added logic for functional-shared / stack

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,38 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+var newObj = {};
+
+newObj.n = 0;
+newObj.storage = {};
+
+_.extend(newObj, stackMethods);
+
+return newObj;
+
 };
 
-var stackMethods = {};
+
+
+
+var stackMethods = {
+
+	push: function(value) {
+		this.n ++;
+		this.storage[this.n] = value;
+	},
+
+	pop: function() {
+		var result = this.storage[this.n];
+		delete this.storage[this.n];
+		if (this.n) { this.n--; }
+		return result;
+	},
+
+	size: function() {
+		return this.n;
+	}	
+
+};
 
 

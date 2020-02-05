67fbfbfd3822814fbdd23fc57dd8cd5d81c8d250
Laura Heddleston <laura.heddleston@gmail.com>
2020-02-05 11:34:51 -0800

    Added logic for functional/stack

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,18 +1,33 @@
 var Stack = function() {
+
   var someInstance = {};
+  var n = 0;
 
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
   someInstance.push = function(value) {
+    // add value to object
+    n++;
+    storage[n] = value;
   };
 
   someInstance.pop = function() {
+    // delete value from object
+    var result = storage[n];
+    delete storage[n];
+    if (n) {
+      n--;
+    }
+    return result;
   };
 
   someInstance.size = function() {
+    // return size of instance
+    return n;
   };
 
   return someInstance;
 };
+

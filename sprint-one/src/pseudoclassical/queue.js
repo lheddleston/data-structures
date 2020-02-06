fa8da21cd7cd5da7f7738c524842a948245e4073
Laura Heddleston <laura.heddleston@gmail.com>
2020-02-06 11:22:32 -0800

    Added logic for pseudoclassical / queue

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,27 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+
+  this.n = 0;
+  this.last = 0;
+  this.storage = {};
+
+};
+
+Queue.prototype.enqueue = function(value) {
+  this.n++;
+  this.storage[this.last] = value;
+  this.last++;
+};
+
+Queue.prototype.dequeue = function() {
+  var start = this.last - this.n;
+  var result = this.storage[start];
+  delete this.storage[start];
+  if (this.n) { this.n--; }
+  return result;
+};
+
+Queue.prototype.size = function() {
+  return this.n;
 };
 
 

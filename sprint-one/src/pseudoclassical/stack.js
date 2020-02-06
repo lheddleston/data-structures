8949812b6341cae8908afe35e2d5d8e985f86b14
Laura Heddleston <laura.heddleston@gmail.com>
2020-02-06 09:16:28 -0800

    Added logic for stack /pseudoclassical

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,25 @@
+// Function should build an object that has the functionality to have values added to the
+// top of the stack, values removed from the stack and return the number of items in the stack
+
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+
+  this.n = 0;
+  this.storage = {};
+
 };
 
+Stack.prototype.push = function(value) {
+  this.n++;
+  this.storage[this.n] = value;
+};
 
+Stack.prototype.pop = function() {
+  var result = this.storage[this.n];
+  delete this.storage[this.n];
+  if (this.n) { this.n--; }
+  return result;
+};
+
+Stack.prototype.size = function() {
+  return this.n;
+};

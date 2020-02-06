a08bf502e20dc89b06e7fbe4ebe7f61fc25a4c44
Laura Heddleston <laura.heddleston@gmail.com>
2020-02-06 11:18:03 -0800

    Added logic for prototypal / queue

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,32 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+
+  var queueObj = Object.create(queueMethods);
+  queueObj.n = 0;
+  queueObj.last = 0;
+  queueObj.storage = {};
+
+  return queueObj;
 };
 
-var queueMethods = {};
+var queueMethods = {
+
+  enqueue: function(value) {
+    this.n++;
+    this.storage[this.last] = value;
+    this.last++;
+  },
+
+  dequeue: function() {
+    var start = this.last - this.n;
+    var result = this.storage[start];
+    delete this.storage[start];
+    if (this.n) { this.n--; }
+    return result;
+  },
+
+  size: function() {
+    return this.n;
+  }
+};
 
 

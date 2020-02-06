f8f6971ed19332943ca342ebbd15d38b0962c717
Laura Heddleston <laura.heddleston@gmail.com>
2020-02-06 10:25:16 -0800

    Added logic for functional / queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,29 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var n = 0;
+  var last = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    // add value to the end of the queue
+    n++;
+    storage[last] = value;
+    last++;
   };
 
   someInstance.dequeue = function() {
+    // remove first value in the queue
+    var first = last - n;
+    var result = storage[first];
+    delete storage[first];
+    if (n) { n--; }
+    return result;
   };
 
   someInstance.size = function() {
+    return n;
   };
 
   return someInstance;

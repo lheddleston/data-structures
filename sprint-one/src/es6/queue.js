88d7dc51b8d6bece80b28c9120ed6d58676ab96d
Laura Heddleston <laura.heddleston@gmail.com>
2020-02-06 11:53:10 -0800

    Added logic for es6 / queue

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,26 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
-  }
+    this.n = 0;
+    this.last = 0;
+    this.storage = {};
 
+  }
+  enqueue(value) {
+    this.n++;
+    this.storage[this.last] = value;
+    this.last++;
+  }
+  dequeue() {
+    var start = this.last - this.n;
+    var result = this.storage[start];
+    delete this.storage[start];
+    if (this.n) { this.n--; }
+    return result;
+  }
+  size() {
+    return this.n;
+  }
 }
+
+

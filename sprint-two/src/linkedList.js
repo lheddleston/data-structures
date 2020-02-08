6029b576bf4f0f873aea2005a1eddc767699c15a
Laura Heddleston <laura.heddleston@gmail.com>
2020-02-07 23:18:30 -0800

    Added linkedList class in functional style

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,17 +1,42 @@
+/*
+Justification: Create a class LinkedList that has 3 methods that behaves
+like a linked list.
+Specifcations: no input, outputs a list object, edge cases - empty list
+Explanation: No input and the function outputs a new list object
+*/
+
 var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
 
   list.addToTail = function(value) {
+    var node = Node(value);
+    if (list.head) {
+      list.tail.next = node;
+      list.tail = node;
+    } else {
+      list.tail = node;
+      list.head = node;
+    }
   };
-
+         
   list.removeHead = function() {
+    var currentHead = list.head;
+    list.head = currentHead.next;
+    return currentHead.value;
   };
 
   list.contains = function(target) {
+    var current = list.head;
+    while (current) {
+      if (current.value === target) {
+        return true;
+      }
+      current = current.next;
+    }
+    return false;
   };
-
   return list;
 };
 

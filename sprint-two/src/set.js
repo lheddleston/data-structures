172aaa1bf521cc95b60d30de3ed41df5ad6b3fd1
Laura Heddleston <laura.heddleston@gmail.com>
2020-02-08 10:42:06 -0800

    Added Set class in prototypal style

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,27 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set.storage = {}; 
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  //add a property to storage object with a string key equal to item
+  this.storage[item] = {};
 };
 
 setPrototype.contains = function(item) {
+  //return boolean reflecting if the value of that item key is undefined
+  return this.storage[item] ? true : false;
 };
 
 setPrototype.remove = function(item) {
+  delete this.storage[item];
+  
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+  O(1) for all
  */
